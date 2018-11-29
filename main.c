6d009099538aa3181191f2ae16628e58471eaa07
nick <nick33omori@gmail.com>
2018-11-29 16:01:24 -0200

    fixed the segfault, added other aspects for the input.

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 #include "Lista.h"
 #include "Grafo.h"
 
+int isDigit(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+int find_less(char *str) {
+    int i = 0;
+    for (;;) {
+        if (str[i] == '<') {
+            return 1;
+        } else if (str[i] == 0) {
+            return 0;
+        }
+        i++;
+    }
+
+}
+
 Grafo initialize_graph(int regs) {
     Grafo g = new_grafo();
     char numbers[100];
@@ -13,12 +30,14 @@ Grafo initialize_graph(int regs) {
         sprintf(numbers, "%d", i);
         grafo_insert_vertex(g, numbers, i);
     }
+    return g;
 }
 
 int main() {
     char buffer[10000];
-    int gnum, regs;
+    int gnum, regs, src, dest;
     int loaded = 0;
+    Grafo g;
 
     for (;;) {
         char *token;
@@ -35,17 +54,38 @@ int main() {
 
             token = strtok(NULL, " ");
             sscanf(token, "%d:", &gnum);
-            printf("Graph %d\n", gnum);
+            //printf("Graph %d\n", gnum);
             loaded = 1;
         } else if (token[0] == 'K') {
             sscanf(token, "K=%d", &regs);
 
-            Grafo g = initialize_graph(regs);
+            g = initialize_graph(regs);
 
-            printf("%d\n", grafo_get_vertex_data(g, "3"));
+        } else if (isDigit(token[0]) && !find_less(token)) {
+            char *str_src = alloc_inicial();
+            str_src = concatena(str_src, token);
+
+            sscanf(token, "%d", &src);
+
+            grafo_insert_vertex(g, str_src, src);
+
+            token = strtok(NULL, " ");
+            token = strtok(NULL, " ");
+
+            while (token != NULL) {
+                int dest;
+                
+                sscanf(token, "%d", &dest);
+
+                grafo_insert_edge(g, str_src, token, -1);
+
+                token = strtok(NULL, " ");
+
+            }
+            
+        } else if (isDigit(token[0]) && find_less(token)) {
 
         } else {
-            return 0;
         }
 
 
